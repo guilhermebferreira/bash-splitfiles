68b216b4eeb857c692e109e5a41027da92de2ecd
Guilherme Ferreira <360guilherme@gmail.com>
2017-08-31 14:07:23 -0300

    additional parameter to range of lines

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # bash-readlines
 
 Bash to read a text file and split according to the desired number of lines
+
+
+Usage:
+
+	./splitbylines.sh entrada.txt 
+
+or
+
+	./splitbylines.sh entrada.txt 5000
--- splitbylines.sh
+++ splitbylines.sh
@@ -2,36 +2,35 @@
 
 #execute o script passando como parametroo arquivo que pretende ler
 #./splitbylines.sh entrada.txt 
+#ou delimitar a quantidade de linhas com o segundo argumento
+#./splitbylines.sh entrada.txt 5000
 
-x=1	#contador de linhas
+x=0	#contador de linhas
 f=1
 cont=500
+prefix='part_'
+sufix=''
 
-
+if [ -z "$2" ]
+  then
+    echo "No argument supplied"
+else
+    cont=$2
+fi
 
 while IFS='' read -r line || [[ -n "$line" ]]; do
 
-     if [[ "$x" == "$cont" ]]; then
-        
-        echo "finish file $f"
+    if [[ "$x" == 0 ]]; then
+        echo 'printing file' $prefix$f'_'$1$sufix.txt 
+    fi
+
+    echo $line >> $prefix$f'_'$1$sufix.txt 
+
+  	x=$(( x+1 ))
+
+    if [[ "$x" == "$cont" ]]; then
         x=0
         f=$(( f+1 ))
-        
     fi
 
-    teste=$?
-    echo $line >> output_$f.txt
-
-    #echo "$line" | dd of=test$f.txt
-
-    if [[ "$teste" == 1 ]]; then
-        
-        echo "Text read from file: $x"
-        
-        
-    fi
-
-
-   
-  	x=$(( x+1 ))
 done < "$1"
